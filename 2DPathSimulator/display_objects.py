6ac9af49b990928b77c712dad593ce645dca63eb
FabrCas <fabriziocasadei97@gmail.com>
2023-03-09 16:01:38 +0100

    created new display object: Text image and Text Box

--- 2DPathSimulator/display_objects.py
+++ 2DPathSimulator/display_objects.py
@@ -3,12 +3,13 @@ import time
 
 # test subclass that inherits the Sprite superclass
 class Rect(pg.sprite.Sprite):
-    def __init__(self, x, y, dim = 50):
+    def __init__(self, x, y, width = 50, height = 50, color = (0,0,0)):
         super().__init__()
-        self.image = pg.Surface((dim, dim))
-        self.image.fill((255, 0, 0))
+        self.image = pg.Surface((width, height))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.type_DO = "rect"
 
 class Button(pg.sprite.Sprite):
     def __init__(self, x, y, width, height, type_button='on'):
@@ -33,30 +34,6 @@ class Button(pg.sprite.Sprite):
         self.last_click = None
         self.type_DO = 'button'
 
-    # def update(self):
-        # # if pg.mouse.get_pressed()[0]: # left mouse key pressed
-        # mouse_pos = pg.mouse.get_pos()
-        # if self.last_click == None:
-        #     cooling_time = 1
-        # else:
-        #     cooling_time = abs(time.time() - self.last_click)
-        # if self.rect.collidepoint(mouse_pos) and (cooling_time >0.2):
-        #     print(f"Button has been pressed at coordinates {mouse_pos}")
-        #     self.last_click = time.time()
-        #     if self.status == 'on':
-        #         self.image = pg.image.load("static/green_bt.png").convert_alpha()
-        #         self.rect = self.image.get_rect()
-        #         self.rect.center = (self.x, self.y)
-        #         self.image = pg.transform.smoothscale(self.image, (self.width, self.height))
-        #         self.status = 'of
-        #
-        #     elif self.status == 'off':
-        #         self.image = pg.image.load("static/red_bt.png").convert_alpha()
-        #         self.rect = self.image.get_rect()
-        #         self.rect.center = (self.x, self.y)
-        #         self.image = pg.transform.smoothscale(self.image, (self.width, self.height))
-        #         self.status = 'on'
-
     def change_status(self):
         # if needed can be used a cooling time variable to reduce the number of switch
 
@@ -68,9 +45,8 @@ class Button(pg.sprite.Sprite):
             # print(f"Button has been pressed at coordinates {mouse_pos}")
         # self.last_click = time.time()
 
-        print('sto qui')
         if self.status == 'on':
-            print('go to off')
+            print('button goes to off')
             self.image = pg.image.load("static/red_bt.png").convert_alpha()
             self.image = pg.transform.smoothscale(self.image, (self.width, self.height))
             self.rect = self.image.get_rect()
@@ -79,13 +55,77 @@ class Button(pg.sprite.Sprite):
             self.update()
 
         elif self.status == 'off':
-            print('go to on')
+            print('button goes to on')
             self.image = pg.image.load("static/green_bt.png").convert_alpha()
             self.image = pg.transform.smoothscale(self.image, (self.width, self.height))
             self.rect = self.image.get_rect()
             self.rect.center = (self.x, self.y)
             self.status = 'on'
             self.update()
+
+
+class Text(pg.sprite.Sprite):
+    def __init__(self, string, x, y, size_font, color):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.size = size_font
+        self.color = color
+        self.type_DO = "text"
+
+        # create font object
+        self.font = pg.font.Font('freesansbold.ttf', self.size)
+
+        # create display object and get its rect
+        self.text = self.font.render(string, True, color)   # text, antialiasing, color, bg color
+        self.rect = self.text.get_rect()
+        self.rect.center = (self.x, self.y)
+        self.image = self.text
+
+class TextBox():
+    def __init__(self,screen, x, y, width, height, size_font, color_text, color_bg):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.size = size_font
+        self.color_text = color_text
+        self.color_bg = color_bg
+        self.type_DO = "text box"
+        self.rect = pg.Rect(self.x,self.y, self.width, self.height)
+
+        # variable to manage filling of the box
+        self.default_text = '*insert text here*'
+        self.text_box = self.default_text
+        self.box_active = True
+
+        # create font object
+        self.font = pg.font.Font('freesansbold.ttf', self.size)
+        self.text = self.font.render(self.text_box, True, self.color_text)
+        self.min_width = self.text.get_width() + 10
+        # self.image = self.text
+
+    def render(self):
+        pg.draw.rect(self.screen, self.color_bg, self.rect)
+        self.text = self.font.render(self.text_box, True, self.color_text)
+        self.screen.blit(self.text, (self.rect.x + 5, self.rect.y + 5))
+        new_width = max(200, self.text.get_width() + 10)
+        if new_width -10  > self.min_width:
+            self.rect.w = new_width
+
+    # def update(self):
+        # self.image = self.text
+
+    def restore_defult(self):
+        self.text_box = self.default_text
+
+
+
+
+
+
+
 
 
 
--- 2DPathSimulator/sim_launcher.py
+++ 2DPathSimulator/sim_launcher.py
@@ -4,7 +4,7 @@ import time
 import os
 import sys
 import math
-from display_objects import Rect, Button
+from display_objects import Rect, Button, Text, TextBox
 
 # definition of constants as default values
 WIDTH_WIN = 640
@@ -14,6 +14,8 @@ Y_WIN = 0
 FPS = 60;
 XC_WIN = lambda : math.ceil(WIDTH_WIN/2)
 YC_WIN = lambda : math.ceil(HEIGHT_WIN/2)
+
+texts = []
 
 def update_win_size():
     global WIDTH_WIN
@@ -44,28 +46,43 @@ def create_DOs():
     elements_group = pg.sprite.Group()
 
     # add Display elements
-    rect = Rect(XC_WIN(), YC_WIN())
-    elements_group.add(rect)
-
+    # rect = Rect(XC_WIN(), YC_WIN(), 100,200,(0,255,0))
+    # elements_group.add(rect)
     return elements_group
 
-def create_UI():
+def create_UI(screen):
     ui_group = pg.sprite.Group()
-    button_on = Button(300,300, 50,50, 'on')
-    button_off = Button(300, 600, 50, 50, 'off')
+    text_boxes = []
+    button_on   = Button(300,300, 50,50, 'on')
+    button_off  = Button(300, 600, 50, 50, 'off')
+    text_sleep  = Text("Sleep: ", x=220, y=300, size_font=20, color=(0, 0, 0))
+    text_box    = TextBox(screen, x=220, y=500, width=200, height= 30, size_font=20,\
+                          color_text=(0, 0, 255), color_bg=(255,0,255))
 
+    text_boxes.append(text_box)
+    ui_group.add(text_sleep)
     ui_group.add(button_on)
     ui_group.add(button_off)
-    return ui_group
-
+    return ui_group, text_boxes
 
 
 def rendering():
+    # initialize the pygame engine, get main display object and clock for the rendering
     screen, clock = initialization()
-    ui_group = create_UI()
+    # create display objects for the UI
+    ui_group, text_boxes = create_UI(screen)
+    # create display objects for the simulation
     elements_group = create_DOs()
 
+    deleting = None; time_delete = time.time()
     while True:
+
+        if deleting is not None:
+            cooldown_delete = time.time() - time_delete
+            if  cooldown_delete > 0.05:
+                deleting.text_box = deleting.text_box[:-1]
+                time_delete = time.time()
+
         # Process player inputs.
         for event in pg.event.get():
 
@@ -86,32 +103,61 @@ def rendering():
             if event.type == pg.MOUSEBUTTONDOWN:
                 print("Mouse clicked")
                 mouse_pos = pg.mouse.get_pos()
+
+                # interact with the buttons
                 for ui_elem in ui_group:
                     if ui_elem.type_DO == 'button' and (ui_elem.rect.collidepoint(mouse_pos)):
                         ui_elem.change_status()
 
-
-
+                    # if ui_elem.type_DO == 'text box':
+                # interact with the input boxes
+                for text_box in text_boxes:
+                    if (text_box.rect.collidepoint(mouse_pos)):
+                        text_box.box_active = True
+                        if text_box.text_box == text_box.default_text:
+                            text_box.text_box = ''
+                        print(text_box.type_DO + ' is activated')
+                    else:
+                        text_box.box_active = False
+                        # text_box.restore_defult()
 
             if event.type == pg.MOUSEBUTTONUP:
                 print("Mouse released")
                 mouse_pos = pg.mouse.get_pos()
                 # if player.rect.collidepoint(pygame.mouse.get_pos()):
 
+            # text input events
+            if event.type == pg.KEYDOWN:
+                for text_box in text_boxes:
+                    if text_box.type_DO == 'text box' and text_box.box_active:
+                        if event.key == pg.K_BACKSPACE:
+                            deleting = text_box
+                            text_box.text_box = text_box.text_box[:-1]
+                        else:
+                            deleting = None
+                            if event.key == pg.K_RETURN: # reset the text in the box
+                                print(text_box.text_box)
+                                # text_box.text_box = ''
+                                text_box.box_active = False
+                                text_box.restore_defult()
+                            else:
+                             text_box.text_box += event.unicode
+
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_BACKSPACE: deleting = None
 
 
         # Logical updates
         ui_group.update()
-        # elements_group.update()
-
-
+        elements_group.update()
 
         # Render graphics
         screen.fill((255, 255, 255))
+        for text_box in text_boxes:
+            text_box.render()
+
         ui_group.draw(screen)
         elements_group.draw(screen)
-
-
 
         # update the screen with the current state of the display objects
         pg.display.flip()
