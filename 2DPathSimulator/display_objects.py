e173d2c9f92b4a3db2054d19621c2c391e813270
FabrCas <fabriziocasadei97@gmail.com>
2023-03-10 14:08:55 +0100

    Adjusted problems with the system console and build the first version of the control panel (front-end funtionalities)

--- 2DPathSimulator/display_objects.py
+++ 2DPathSimulator/display_objects.py
@@ -1,15 +1,18 @@
 import pygame as pg
 
-progDos_rects = 0
-progDos_static_images = 0
-progDos_buttons = 0
-progDos_texts = 0
-progDos_inputTextBoxes = 0
+
+# ---------------------------------------------- [UI]
+
+progDos_rects           = 1
+progDos_static_images   = 1
+progDos_buttons         = 1
+progDos_texts           = 1
+progDos_TextBoxes       = 1
 
 font_path = "static/nasa_font.ttf"
 font_size = 30
 
-# test subclass that inherits the Sprite superclass
+# simple rect subclass that inherits the Sprite superclass
 class Rect(pg.sprite.Sprite):
 
     def __init__(self, x, y, width = 50, height = 50, color = (0,0,0)):
@@ -48,13 +51,14 @@ class StaticImage(pg.sprite.Sprite):
 
 class Button(pg.sprite.Sprite):
 
-    def __init__(self, x, y, width, height, type_button='on'):
+    def __init__(self, name ,  x, y, width, height, type_button='on'):
 
         super().__init__()
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        self.name = name
         self.status = type_button
         # image = pg.image.load("static/buttons_.png").convert_alpha() # sprite for both on and off button
         # # crop image to get green
@@ -75,7 +79,7 @@ class Button(pg.sprite.Sprite):
         self.prog = progDos_buttons
         progDos_buttons += 1
 
-        print(f"Created {self.type_DO} n° {self.prog}")
+        print(f"Created {self.type_DO} ({self.name}) n° {self.prog}")
 
     def change_status(self):
         # if needed can be used a cooling time variable to reduce the number of switch
@@ -89,7 +93,7 @@ class Button(pg.sprite.Sprite):
         # self.last_click = time.time()
 
         if self.status == 'on':
-            print('button goes to off')
+            print(f'button {self.name} goes to off')
             self.image = pg.image.load("static/red_bt.png").convert_alpha()
             self.image = pg.transform.smoothscale(self.image, (self.width, self.height))
             self.rect = self.image.get_rect()
@@ -98,7 +102,7 @@ class Button(pg.sprite.Sprite):
             self.update()
 
         elif self.status == 'off':
-            print('button goes to on')
+            print(f'button {self.name} goes to on')
             self.image = pg.image.load("static/green_bt.png").convert_alpha()
             self.image = pg.transform.smoothscale(self.image, (self.width, self.height))
             self.rect = self.image.get_rect()
@@ -127,10 +131,22 @@ class Text(pg.sprite.Sprite):
         # create display object and get its rect
         self.text = self.font.render(string, True, color)   # text, antialiasing, color, bg color
         self.rect = self.text.get_rect()
-        self.rect.center = (self.x, self.y)
+        # self.rect.center = (self.x, self.y)
+        self.rect.x = self.x
+        self.rect.y = self.y
         self.image = self.text
 
         print(f"Created {self.type_DO} n° {self.prog}")
+
+    def center_to(self, x = None ,y = None):
+        if x == None and y == None:
+            return
+        elif x == None:
+            self.rect.center = (self.rect.center[0], y)
+        elif y == None:
+            self.rect.center = (x, self.rect.center[1])
+        else:
+            self.rect.center = (x, y)
 
 
 class InputTextBox():
@@ -144,15 +160,16 @@ class InputTextBox():
         self.size = size_font
         self.color_text = color_text
         self.color_bg = color_bg
-        self.type_DO = "text box"
-        global progDos_inputTextBoxes
-        self.prog = progDos_inputTextBoxes
-        progDos_inputTextBoxes += 1
+        self.type_DO = "input text box"
+
+        global progDos_TextBoxes
+        self.prog = progDos_TextBoxes
+        progDos_TextBoxes += 1
 
         # variable to manage filling of the box
         self.default_text = '*insert text here*'
         self.text_box = self.default_text
-        self.box_active = True
+        self.box_active = False
 
         # create font object
         self.font = pg.font.Font(font_path, self.size)
@@ -160,14 +177,14 @@ class InputTextBox():
         self.min_width = self.text.get_width() + 10   # minimum length when it's used the default text
         self.rect = pg.Rect(self.x, self.y, self.min_width, self.height)
 
-        print(f"Created {self.type_DO} n° {self.prog}")
+        print(f"Created text box (input) n° {self.prog}")
 
 
     def render(self, max_width=200):
         pg.draw.rect(self.screen, self.color_bg, self.rect)
         self.text = self.font.render(self.text_box, True, self.color_text)
 
-        new_width = min(max_width, self.text.get_width())
+        new_width = min(max_width, self.text.get_width() + 10)
 
         # stretch the box until the limit is reached
         if new_width > self.min_width:
@@ -186,6 +203,136 @@ class InputTextBox():
     def restore_defult(self):
         self.rect.w = self.min_width
         self.text_box = self.default_text
+
+class OutputTextBox():
+
+    def __init__(self,screen, x, y, width, height, color_text, color_bg, size_font=font_size):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.size = size_font
+        self.color_text = color_text
+        self.color_bg = color_bg
+        self.type_DO = "output text box"
+
+        global progDos_TextBoxes
+        self.prog = progDos_TextBoxes
+        progDos_TextBoxes += 1
+
+        self.messages = []
+        self.default_text = ''
+        self.text_box = ''
+
+        # create font object
+        self.font = pg.font.Font(font_path, self.size)
+        self.text = self.font.render(self.text_box, True, self.color_text)
+        self.rect = pg.Rect(self.x, self.y, self.width, self.height)
+
+        print(f"Created text box (output) n° {self.prog}")
+
+
+
+
+    def render(self):
+
+        # draw the rect containing the text
+        pg.draw.rect(self.screen, self.color_bg, self.rect)
+
+        # define the max height, not all the messages can be visible, show the most recent until there is space
+        max_height = self.rect.height - 15
+        messages_height = 0
+        visible_messages = []
+        n_messages = 0
+        while messages_height < max_height and n_messages +1 <= len(self.messages):
+            message = self.messages[- (n_messages +1)]
+            # print(message)
+            if type(message) == str:
+                message_height = self.font.render(message, True, self.color_text).get_height()
+                messages_height += message_height
+            elif type(message) == list:
+                sub_messages_height = 0
+                for sub_message in message:
+                    sub_message_height = self.font.render(sub_message, True, self.color_text).get_height()
+                    sub_messages_height += sub_message_height
+                messages_height += sub_messages_height
+
+            if messages_height < max_height:
+                n_messages += 1
+                visible_messages.insert(0, message)
+
+        # todo
+
+
+        # print messages as lines after the split
+        vertical_offset = 0
+        for idx, message in enumerate(visible_messages):
+            if type(message) == str:
+                self.text = self.font.render(message, True, self.color_text)
+                self.screen.blit(self.text, (self.rect.x + 5, self.rect.y + 5 + 25 *vertical_offset))
+                vertical_offset += 1
+            elif type(message) == list:
+                for sub_messsage in message:
+                    self.text = self.font.render(sub_messsage, True, self.color_text)
+                    self.screen.blit(self.text, (self.rect.x + 5, self.rect.y + 5 + 25 * vertical_offset))
+                    vertical_offset += 1
+
+
+
+            # break
+
+    def add_message(self, message):
+        # define max width for the box
+        max_width = self.rect.w - 15
+
+        message = '- '+ message[0].upper() + message[1:]
+
+        # create sub-messages if text is too long
+        message_text = self.font.render(message, True, self.color_text)
+        if message_text.get_width() + 10 > max_width: # sub-messages are needed
+            sub_messages = []
+
+            while (message != ''):
+                sub_message = message
+                words = sub_message.split(' ')
+                sub_message_text = self.font.render(sub_message, True, self.color_text)
+
+                while (sub_message_text.get_width() > max_width):
+                    sub_messages_list = sub_message.split(' ')
+                    sub_message = ' '.join(sub_messages_list[:-1])
+
+                    # sub_message = sub_message.replace(words[-1], '')
+                    sub_message_text = self.font.render(sub_message, True, self.color_text)
+
+                message = message.replace(sub_message, '').strip()
+                if sub_messages != []:
+                    sub_message = '   ' + sub_message
+                sub_messages.append(sub_message)
+
+            self.messages.append(sub_messages)
+        else:
+            self.messages.append( message)
+
+    def restore_defult(self):
+        self.text_box = self.default_text
+# ---------------------------------------------- [Simulation]
+
+class Room():
+    def __init__(self):
+        pass
+
+class Pepper():
+    def __init__(self):
+        pass
+
+class HouseObject():
+    def __init__(self):
+        pass
+
+class Furniture():
+    def __init__(self):
+        pass
 
 
 
--- 2DPathSimulator/sim_launcher.py
+++ 2DPathSimulator/sim_launcher.py
@@ -4,8 +4,9 @@ import time
 import os
 import sys
 import math
-import random
-from display_objects import Rect, Button, Text, InputTextBox, StaticImage
+import threading
+import asyncio
+from display_objects import Rect, Button, Text, InputTextBox, StaticImage, OutputTextBox
 
 # definition of constants as default values
 WIDTH_WIN = 640
@@ -58,44 +59,81 @@ def create_DOs():
     return elements_group
 
 def create_UI(screen):
+    # create collections
     ui_group = pg.sprite.Group()
     text_boxes = []  # text boxes are handled differently since are custom objects that don't inherit pygame classes
 
-    # lateral panel
-
     # print(screen.get_width(), screen.get_height())
 
-
-    lateral_panel =  StaticImage(file_path = "static/lateral_panel.jpg", x= math.ceil(screen.get_width()*2/3), y =0, \
-                                 width= math.ceil(screen.get_width()/3), height= screen.get_height())
-                                 # width=1000, height=1000)
-    button_on   = Button(300,300, 50,50, 'on')
-    button_off  = Button(300, 600, 50, 50, 'off')
-    text_sleep  = Text("Sleep: ", x=220, y=300,  color=(0, 255, 0), size_font=20)
-    text_box    = InputTextBox(screen, x=220, y=500,height= 30,
-                               color_text=(0, 0, 0), color_bg=(160,160,160),size_font=20)
-    x_r, y_r = random_position(screen)
-    text_box2    = InputTextBox(screen, x_r, y_r, height= 30,
-                                color_text=(0, 0, 0), color_bg=(160,160,160), size_font=20)
-
-    text_boxes.append(text_box)
-    text_boxes.append(text_box2)
+    # 1) create lateral panel and title
+    # ltc -> left top corner
+    lateral_panel_ltc_x = math.ceil(screen.get_width()*2/3)
+    lateral_panel_ltc_y = 0
+    lateral_panel_width = math.ceil(screen.get_width()/3)
+    lateral_panel_height = screen.get_height()
+    lateral_panel =  StaticImage(file_path = "static/lateral_panel.jpg", x= lateral_panel_ltc_x,
+                                 y =lateral_panel_ltc_y, width= lateral_panel_width, height= lateral_panel_height)
+    text_control_panel = Text("Control Panel", x= lateral_panel_ltc_x
+                              , y=10,  color=(255, 255, 255), size_font=30)
+    text_control_panel.center_to(x = lateral_panel_ltc_x + lateral_panel_width/2)
 
     ui_group.add(lateral_panel)
-    ui_group.add(text_sleep)
-    ui_group.add(button_on)
-    ui_group.add(button_off)
+    ui_group.add(text_control_panel)
 
-    """
-    - prompt command 
-    - reset button 
-    - sleep button 
-    - simulator messages
-    """
+    # 2) create prompt
+    text_prompt = Text("Prompt ", x=lateral_panel_ltc_x + 10, y=90,  color=(255, 255, 255), size_font=20)
+    prompt    = InputTextBox(screen, x=lateral_panel_ltc_x + 135, y=85, height= 30,
+                               color_text=(0, 0, 0), color_bg=(200,200,200),size_font=20)
+
+    ui_group.add(text_prompt)
+    text_boxes.append(prompt)
+
+    # 3) create buttons
+    text_sleep  = Text("Sleep mode ", x=lateral_panel_ltc_x + 10, y=170,  color=(255, 255, 255), size_font=20)
+    button_sleep = Button("sleep",lateral_panel_ltc_x + lateral_panel_width*3/5, 170 + 15, 60, 60, 'off')
+
+    text_mic  = Text("Microphone ", x=lateral_panel_ltc_x + 10, y=250,  color=(255, 255, 255), size_font=20)
+    button_mic = Button("microphone", lateral_panel_ltc_x + lateral_panel_width*3/5, 250 + 15, 60, 60, 'on')
+
+    text_extraHUD = Text("Extra HUD ", x=lateral_panel_ltc_x + 10, y=330,  color=(255, 255, 255), size_font=20)
+    button_extraHUD = Button('HUD', lateral_panel_ltc_x + lateral_panel_width*3/5, 330 + 15, 60, 60, 'off')
+
+    ui_group.add(text_sleep)
+    ui_group.add(button_sleep)
+    ui_group.add(text_mic)
+    ui_group.add(button_mic)
+    ui_group.add(text_extraHUD)
+    ui_group.add(button_extraHUD)
+
+    # 4) create system Output box
+    system_box = OutputTextBox(screen, x= lateral_panel_ltc_x + 10, y= lateral_panel_height - 410, width=lateral_panel_width -20, height= 400,
+                               color_text=(0, 0, 0), color_bg=(200,200,200),size_font=20)
+
+    text_system_box = Text("System output", x=lateral_panel_ltc_x + 10, y= system_box.y-30,  color=(255, 255, 255), size_font=20)
+    text_system_box.center_to(x = lateral_panel_ltc_x + lateral_panel_width/2)
+    text_boxes.append(system_box)
+    ui_group.add(text_system_box)
+
+    # test inserendo i messaggi
+    system_box.add_message("primo messaggio")
+    system_box.add_message("secondo messaggio")
+    system_box.add_message("prova prova prova")
+    system_box.add_message("sa")
+    system_box.add_message("prova prova prova")
+    system_box.add_message("sa")
+    system_box.add_message(
+        "Messaggio molto lungo anzi lunghissimo direi bislungo, ma esiste veramente come parola 'bislungo'? Ma alla fine che ne so io penso di si boh")
+    system_box.add_message("prova prova prova")
+    system_box.add_message("Bla bla bla bla bla blu blue blue")
+    system_box.add_message("Messaggio molto lungo, anzi lunghissimo direi bislungo, ma esiste veramente come parola 'bislungo'? Ma alla fine che ne so io penso di si boh")
+
+    start_time = threading.Timer(3, lambda : system_box.add_message("Nuovo messaggio"))
+    start_time.start()
 
     return ui_group, text_boxes
 
 def rendering():
+
     # initialize the pygame engine, get main display object and clock for the rendering
     screen, clock = initialization()
     # create display objects for the UI
@@ -104,14 +142,17 @@ def rendering():
     elements_group = create_DOs()
 
     # variable used for the continuous elimination of characters when pressing backspace
-    deleting = None; time_delete = time.time();
+    deleting = None; time_delete = time.time(); deleting_wait = 0.15
+
+    pressed_sx_mouse = False
 
     # ---------------------------------------- Main rendering Loop
     while True:
 
         if deleting is not None:
             cooldown_delete = time.time() - time_delete
-            if  cooldown_delete > 0.15:
+            if  cooldown_delete > deleting_wait:
+                deleting_wait *= 0.7
                 deleting.text_box = deleting.text_box[:-1]
                 time_delete = time.time()
 
@@ -135,7 +176,8 @@ def rendering():
                     return
 
             # Mouse click events
-            if event.type == pg.MOUSEBUTTONDOWN:
+            if event.type == pg.MOUSEBUTTONDOWN and pg.mouse.get_pressed()[0]:
+                pressed_sx_mouse = True
                 print("Mouse clicked")
                 mouse_pos = pg.mouse.get_pos()
 
@@ -146,26 +188,28 @@ def rendering():
 
                 # interact with the input boxes
                 for box in text_boxes:
-                    if (box.rect.collidepoint(mouse_pos)):
-                        box.box_active = True
-                        if box.text_box == box.default_text:
-                            box.text_box = ''
-                        print(box.type_DO + ' is activated')
-                    else:
-                        box.box_active = False
-                        if box.text_box.strip() == '':
-                            box.restore_defult()
+                    if box.type_DO == "input text box":
+                        if box.rect.collidepoint(mouse_pos):
+                            box.box_active = True
+                            if box.text_box == box.default_text:
+                                box.text_box = ''
+                            print(box.type_DO + ' is activated')
+                        else:
+                            if box.box_active:
+                                box.box_active = False
+                                print(box.type_DO + ' is deactivated')
+                                if box.text_box.strip() == '':
+                                    box.restore_defult()
 
-            if event.type == pg.MOUSEBUTTONUP:
+            if event.type == pg.MOUSEBUTTONUP and (pressed_sx_mouse and not(pg.mouse.get_pressed()[0])):
                 print("Mouse released")
-                mouse_pos = pg.mouse.get_pos()
-                # if player.rect.collidepoint(pygame.mouse.get_pos()):
+                pressed_sx_mouse = False
 
             # text input events
             if event.type == pg.KEYDOWN:
                 for box in text_boxes:
-                    if box.type_DO == 'text box' and box.box_active:
-                        if event.key == pg.K_BACKSPACE:
+                    if 'input text box' in box.type_DO and box.box_active:
+                        if event.key == pg.K_BACKSPACE:  # start the deleting, save time and box where remove chars
                             time_delete = time.time()
                             deleting = box
                             box.text_box = box.text_box[:-1]
@@ -173,14 +217,14 @@ def rendering():
                         elif event.key == pg.K_RETURN: # reset the text in the box
                             deleting = None
                             print(box.text_box)
-                            # text_box.text_box = ''
                             box.box_active = False
                             box.restore_defult()
 
-                        elif event.unicode.isprintable():
+                        elif event.unicode.isprintable(): # insert unicode chars in the prompt
                             deleting = None
                             box.text_box += event.unicode
 
+                        deleting_wait = 0.15  # reset the velocity when deleting characters from the prompt
 
             if event.type == pg.KEYUP:
                 if event.key == pg.K_BACKSPACE: deleting = None
